c78433edd4de7ab3b2cc87ca0afd93438589000a
NatashaPatricia <122792084+NatashaPatricia@users.noreply.github.com>
2023-01-27 22:36:25 +0300

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,5 @@
+#program to calculate the area of a circle
+pie=3.142
+radius=int(input("Enter the radius: "))
+area=pie*radius*radius
+print(area)
---
+++ hello.py
@@ -0,0 +1,26 @@
+#Introduction to python
+# programming in python
+'''
+author:talia
+publisher: lomghorn
+year:9876
+'''
+print("Hello World")
+#boolean
+print(True)
+#float
+a=456.6788
+print(a)
+
+#sSring
+name='python'
+print (name)
+x = 10
+y = 7
+
+print(x==y)
+print(x>y)
+print(x<y)
+print(x<=y)
+print(x>=y)
+print(x>=y)
